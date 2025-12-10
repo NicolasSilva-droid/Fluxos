69d8dff9a85668ac98ffed110b69eebfa90bb8aa
Nicolas Alexandre da Silva <nicolas19388108@gmail.com>
2025-12-10 18:04:59 -0300

    Refactor checkout functions into an object

--- cypress/support/checkout.js
+++ cypress/support/checkout.js
@@ -1,27 +1,32 @@
-export function adicionarProdutoAoCarrinho() {
-    cy.get('[data-testid="category-Categoria Requisito 1764601228266"]').click()
-    cy.get('[data-testid="productDetails"]').click();
-    cy.get('[data-testid="addToCart"]').click();
+export const checkout = {
 
+    adicionarProdutoAoCarrinho() {
+        cy.get('[class="css-175oi2r r-1i6wzkk r-lrvibr r-1loqt21 r-1otgn73"]').click();
+        cy.get('[data-testid="addToCart"]').click();
+        cy.get('[data-testid="selectAddressOrContinueToPayment"]').click();
+        cy.contains('[data-testid="goBackHome"]').should('Go Back Home')
+    },
+    preencherCheckout(nome, telefone, endereco, cidade, estado, cep) {
+        cy.get('[data-testid="addNewAddress"]').click();
+        cy.get('[placeholder="Enter your name"]').type(nome);
+        cy.get('[placeholder="Enter your mobile number"]').type(telefone);
+        cy.get('[placeholder="Enter your address"]').type(endereco);
+        cy.get('[placeholder="City"]').type(cidade);
+        cy.get('[placeholder="State"]').type(estado);
+        cy.get('[placeholder="ZipCode"]').type(cep);
+        cy.get('[data-testid="save"]').click();
+        cy.get('[data-testid="goBackHome"').should()
+    },
+    removerProdutoCheckout(){
+        cy.get('[class="css-175oi2r r-1i6wzkk r-lrvibr r-1loqt21 r-1otgn73"]').click();
+        cy.get('[data-testid="addToCart"]').click();
+        cy.get('[data-testid="remove"]').click();
+        cy.contains('[data-testid="emptyCart"]').should()
+    },
+    atualizarCheckout(){
+        cy.get('[class="css-146c3p1 r-lrvibr"]').click();
+        cy.get('[data-testid="back"]').click();
+        cy.get('[class="css-146c3p1 r-lrvibr"]').click();
+
+    }
 }
-
-export function preencherCheckout(nome, telefone, endereco, cidade, estado, cep) {
-    cy.get('[data-testid="addNewAddress"]').click();
-    cy.get('[placeholder="Enter your name"]').type(nome);
-    cy.get('[placeholder="Enter your mobile number"]').type(telefone);
-    cy.get('[placeholder="Enter your address"]').type(endereco);
-    cy.get('[placeholder="City"]').type(cidade);
-    cy.get('[placeholder="State"]').type(estado);
-    cy.get('[placeholder="ZipCode"]').type(cep);
-    cy.get('[data-testid="save"]').click();
-
-}
-
-export function irParaCarrinho() {
-  cy.get('[data-testid="selectAddressOrContinueToPayment"]').click()
-}
-
-export function finalizarCompra() {
-    cy.get('[data-testid="completeCheckout"]').click()
-}
-
