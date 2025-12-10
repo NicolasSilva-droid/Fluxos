7940267138314a190d57026546044cc6e83c6e23
Nicolas Alexandre da Silva <nicolas19388108@gmail.com>
2025-12-10 18:10:02 -0300

    Update checkout.fluxo.js

--- cypress/e2e/checkout.fluxo.js
+++ cypress/e2e/checkout.fluxo.js
@@ -1,16 +1,12 @@
 import {
   adicionarProdutoAoCarrinho,
-  preencherCheckout,
-  irParaCarrinho,
-  finalizarCompra
+  preencherCheckout
 } from '../support/checkout';
 
 describe('fluxo de checkout', () => {
   it('deve realizar o checkout com sucesso', () => {
     adicionarProdutoAoCarrinho(); 
     preencherCheckout('Alberto', 48993468152, 'rua jardim', 'metropolis', 'santa rica', '44471-000');
-    irParaCarrinho();
-    finalizarCompra();
     cy.contains('[data-testid="goBackHome"]').should('Go Back Home');
   });
 });
